67dfe960a101be1a979a87e50020011a61db5a52
tracybrtn <tracynburton@gmail.com>
2022-09-16 09:54:08 -0500

    passing tests for employee

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -4,7 +4,7 @@ const Employee = require('../lib/Employee.js');
 
 //Employee is a parent class that should have the following classes and methods
 it('creates an Employee object', () => {
-    const employee = new Employee('Tracy');
+    const employee = new Employee('Tracy', 8, 'email');
 
     //test for name, id, and email
     expect(employee.name).toBe('Tracy');
@@ -14,21 +14,21 @@ it('creates an Employee object', () => {
 
 //getId()
 it('gets id value for employee', () => {
-    const employee = new Employee('Tracy');
+    const employee = new Employee('Tracy', 8, 'email');
 
-    expect(employee.getId()).toEqual(expect.stringContaining(employee.id.toString()));
+    expect(employee.getId()).toEqual(employee.id);
 })
 
 //getEmail()
 it('gets email value for employee', () => {
-    const employee = new Employee('Tracy');
+    const employee = new Employee('Tracy', 8, 'email');
 
     expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email));
 })
 
 //getRole() which returns 'Employee'
 it('gets role value for employee', () => {
-    const employee = new Employee('Tracy');
+    const employee = new Employee('Tracy', 8, 'email');
 
     expect(employee.getRole()).toBe('Employee');
 })
